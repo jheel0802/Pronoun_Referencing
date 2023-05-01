2bf94646df19c3e5d370a028892ec6a13a82ad24
jheel0802 <jheel0802@gmail.com>
2023-05-01 18:06:20 +0530

    noun part works

--- Mini project_modified/server.py
+++ Mini project_modified/server.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 import stanza
-import spacy
-from comma_and_separated_nouns import get_noun_phrases
 
 # download the English language model
 stanza.download('en')
@@ -27,42 +25,41 @@ def index():
 def extract_pronouns(text):
     # process the input text
     doc = nlp(text)
-
     # extract the pronouns from the processed text
     pronouns = []
     for sentence in doc.sentences:
         for word in sentence.words:
             if word.upos == 'PRON':
                 pronouns.append(word.text)
-
     # return the identified pronouns as a list
     return pronouns
 
 def get_noun_phrases(text):
-    # process the input text
+    noun_phrases = []
+    # process the sentence
     doc = nlp(text)
 
-    # extract the noun phrases from the processed text
-    noun_phrases = []
-    for sentence in doc.sentences:
-        phrase = []
-        for token in sentence.tokens:
-            # check if the current token is a noun
-            if token.upos.startswith('N'):
-                phrase.append(token.text)
-            # if the current token is not a noun, add the current phrase to the list of phrases
-            elif phrase:
-                noun_phrases.append(' '.join(phrase))
-                phrase = []
-        # if the sentence ends with a noun phrase, add it to the list of phrases
-        if phrase:
-            noun_phrases.append(' '.join(phrase))
-
-    # return the identified noun phrases as a list
+    # start the reverse traversal from the end of the sentence
+    i = len(doc.sentences[0].words) - 1
+    while i >= 0:
+        # if a word has deprel "conj", start a second pointer j to find the subject deprel
+        if doc.sentences[0].words[i].deprel == "conj":
+            j = i - 1
+            while j >= 0:
+                # if a subject deprel is found, print the words from j to i
+                if doc.sentences[0].words[j].deprel.startswith("obl") or doc.sentences[0].words[j].deprel.startswith("nsubj") or doc.sentences[0].words[j].deprel.startswith("obj"):
+                    words = [word.text for word in doc.sentences[0].words[j:i+1]]
+                    noun_phrases.append(" ".join(words))
+                    print("hit4")
+                    print(noun_phrases)
+                    break
+                j -= 1
+            # start the next traversal from the position before the subject deprel word
+            i = j - 1
+        else:
+            i -= 1
+    print("return:",noun_phrases)
     return noun_phrases
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
